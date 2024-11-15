c4b0f21b2ad7e6f5e0104d210ca32f00ffb5f5bd
AbrarRamadava <114666990+AbrarRamadava@users.noreply.github.com>
2024-11-16 02:23:07 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<script>
+import CommentList from './components/CommentList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CommentList
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>Tambahkan Komentar</h1>
+    <CommentList />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Arial', sans-serif;
+  background-color: #f9f9f9;
+  padding: 30px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 24px;
+  color: #333;
+  margin-bottom: 20px;
+}
+</style>
